12e75bed42afd9d8a75184ee073a322ab6ab01e1
Aurigain <shubham.matta@aurigain.com>
2022-04-05 14:05:44 +0530

    home module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+    // path: '',
+    // loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+];
 
 @NgModule({
   imports: [
