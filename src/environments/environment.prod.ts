3f6679044d71c4a503deb1fde6ada4579afe7c12
Aurigain <shubham.matta@aurigain.com>
2022-07-13 11:28:22 +0530

    base url ch1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // BASE_URL: 'http://localhost:8000'
   // BASE_URL: 'http://127.0.0.1:8000',
   // BASE_URL: 'http://aurigainappdeployment-env.eba-vhsamhp3.ap-south-1.elasticbeanstalk.com'
-  BASE_URL: 'https://oyster-app-tfv95.ondigitalocean.app/'
+  BASE_URL: 'https://oyster-app-tfv95.ondigitalocean.app'
 };
 
 
